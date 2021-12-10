314f23abd384b2a7189b6729f1d3ca43dcf56699
tony <antolisah@gmail.com>
2021-12-10 13:06:47 +0300

    widget service

--- lib/my_widget_service-stack.ts
+++ lib/my_widget_service-stack.ts
@@ -1,15 +1,12 @@
 import * as cdk from '@aws-cdk/core';
 // import * as sqs from '@aws-cdk/aws-sqs';
 
+import * as widget_service from '../lib/widget_service'
+
 export class MyWidgetServiceStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyWidgetServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new widget_service.WidgetService(this, 'Widgets');
   }
 }
